e04ff7dd206eb13d67518a42ee67957fa5c8cea1
Kopejkin Dmitrij <dmitrij.kopejkin@privatbank.ua>
2021-10-24 16:36:40 +0300

    hw3

---
+++ src/com/pb/kopejkin/hw3/Array.java
@@ -0,0 +1,35 @@
+package com.pb.kopejkin.hw3;
+
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        System.out.println("Поочередно введите целые значения массива ");
+        Scanner console = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(i+1 + ": ");
+            array[i] = console.nextInt();
+        }
+
+        System.out.println("Наш массив:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+
+        for (int i = 0; i < (array.length-1); i++){
+            for (int j =0; j<(array.length-1-i); j++){
+                if (array[j] > array[j+1]){
+                    int tmp = array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=tmp;
+                }
+            }
+        }
+
+        System.out.println(" \nНаш массив после сортировки:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
+    }
+}
